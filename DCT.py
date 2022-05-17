f6f9fffcb8a2993aeae74d77d29f4b20f9c15f7a
eclerchik13 <ler.poter2014@yandex.ru>
2022-05-17 15:30:46 +0300

    Add parameter, which choose method "change block" for sender and recipient

--- DCT.py
+++ DCT.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 import math
 import pandas as pd
 
+import GA
 import KochAndZhao
 import SeparateFunc
 
 n = 8
-#mark = 25
 original = io.imread('forest.png')
 original_2 = io.imread('https://www.miruma.ru/thumbs/7e8475989_160x160.jpg')
 u1, v1 = 4, 5
@@ -26,7 +26,7 @@ def get_dct2(block):
 def get_idct2(coifs):
     block = idct(idct(coifs, axis=0, norm='ortho'), axis=1, norm='ortho')
     out_block = double_to_byte(block)
-    #out_block = normalization(block)
+    # out_block = normalization(block)
     return out_block
 
 
@@ -39,7 +39,7 @@ def normalization(arr):  # block        # с ней хуже, изображен
     min_element = np.amin(arr)
     arr += abs(min_element)
     max_element = np.amax(arr)
-    arr_norm = arr/max_element * 255
+    arr_norm = arr / max_element * 255
     return arr_norm
 
 
@@ -70,7 +70,7 @@ def create_blocks(channel):
     return blocks
 
 
-def PSNR(original_img, compressed):     # total psnr
+def PSNR(original_img, compressed):  # total psnr
     mse = np.mean((original_img - compressed) ** 2)
     if mse == 0:
         print("psnr=80")
@@ -88,16 +88,64 @@ def blue_channel_fusion(blue_channel, size_img, change_blocks):
     return copy_blue
 
 
-if __name__ == "__main__":
+def fitness_func(chrom, block, img_block):  # CHANGING PARAMETERS
+    block_ = block.copy()
+    x1, x2 = chrom[0][0], chrom[0][1]
+    y1, y2 = chrom[1][0], chrom[1][1]
+    block_[x1][x2], block_[y1][y2] = KochAndZhao.change_block(3, True, block_[x1][x2], block_[y1][y2])
+    block_change_img = get_idct2(block_)
+    psnr_value_of_chrom = PSNR(img_block, block_change_img)
+    return psnr_value_of_chrom
 
+
+def find_coefs_for_koch(h1, h2, dct_blocks, original_blocks):
+    dct_blocks_copy = dct_blocks.copy()
+    arr_coefs = np.empty([h1, h2, 2, 2])
+    for i in range(h1):
+        for j in range(h2):
+            coefs, psnr_score = GA.ga_for_block(dct_blocks_copy[i][j], original_blocks[i][j], fitness_func,
+                                                n=8, n_pop=50, n_iter=12, size_tour=7, p_cross=0.89, p_mut=0.09)
+            # GA for each block, return coefs
+            arr_coefs[i, j] = coefs
+            print('psnr of blocks', psnr_score, 'blocks #', i, j)
+    return arr_coefs
+
+
+if __name__ == "__main__":
     change = original_2.copy()
     blue = change[:, :, 2]  # выделение синего канала
     blocks_image = create_blocks(blue)  # массив блоков синего канала (изображения)
     dct_all_blocks = get_dct_blocks(blocks_image)  # массив блоков ДКП
-    h_1 = dct_all_blocks.shape[0]   # кол-во блоков
+    h_1 = dct_all_blocks.shape[0]  # кол-во блоков
 
-    first_coefs = KochAndZhao.find_coefs_for_koch(h_1, h_1, dct_all_blocks, blocks_image)
-    SeparateFunc.save_coef('1coef.npy', first_coefs)     # сохранение первых найденных коэффициентов
+    """Поиск и Сохранение первых коэффициентов для Коха и Жао"""
+
+    # first_coefs = find_coefs_for_koch(h_1, h_1, dct_all_blocks, blocks_image)
+    # SeparateFunc.save_coef('1coef.npy', first_coefs)     # сохранение первых найденных коэффициентов
+
+    """Выгрузка первых коэффициентов для Коха и Жао с файла"""
+
+    first_coefs_from_file = SeparateFunc.get_saved_coef('1coef.npy')
+
+    """Встраивание "1" и "0" в выбранные коэффициенты => Два массива с ДКП блоками"""
+
+    #dct_with_one, dct_with_null = KochAndZhao.hiding_bits(h_1, n, dct_all_blocks, first_coefs_from_file)
+
+    """Поиск и Сохранение вторых коэффициентов для Коха и Жао для блоков с "1" и с "0" => Нужны их ОДКП для ГА"""
+    """
+    idct_blocks_with_one = get_idct_blocks(dct_with_one)    # Нужны их ОДКП для ГА
+    idct_blocks_with_null = get_idct_blocks(dct_with_null)
+
+    second_coefs_one = find_coefs_for_koch(h_1, h_1, dct_with_one, idct_blocks_with_one)
+    SeparateFunc.save_coef('2coef_for_one.npy', second_coefs_one)     # сохранение вторых найденных коэффициентов для 1
+
+    second_coefs_null = find_coefs_for_koch(h_1, h_1, dct_with_null, idct_blocks_with_null)
+    SeparateFunc.save_coef('2coef_for_null.npy', second_coefs_null)  # сохранение вторых найденных коэффициентов для 0
+`   """
+    """Выгрузка вторых коэффициентов для Коха и Жао с файлов"""
+
+    second_coefs_from_file_one = SeparateFunc.get_saved_coef('2coef_for_one.npy')
+    second_coefs_from_file_null = SeparateFunc.get_saved_coef('2coef_for_null.npy')
 
     idct_all_blocks = get_idct_blocks(dct_all_blocks)
     blue_changed = blue_channel_fusion(blue, h_1, idct_all_blocks)
