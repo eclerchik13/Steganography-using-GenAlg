cc6868cfc818cb5b941b1b4452a66c0ed8be871e
eclerchik13 <ler.poter2014@yandex.ru>
2022-05-20 13:33:53 +0300

    Add identification of blocks suitable for the method Koch and Zhao for embedding, block processing time, showing suitable blocks, hiding bit with method Koch and Zhao, calculation PSNR image with bit with only Koch and Zhao

--- DCT.py
+++ DCT.py
@@ -3,8 +3,9 @@ from skimage import io
 from skimage.util import view_as_blocks
 from scipy.fftpack import dct, idct
 import matplotlib.pyplot as plt
+# from image_slicer import slice
 import math
-import pandas as pd
+import time
 
 import GA
 import KochAndZhao
@@ -12,7 +13,7 @@ import SeparateFunc
 
 n = 8
 original = io.imread('forest.png')
-original_2 = io.imread('https://www.miruma.ru/thumbs/7e8475989_160x160.jpg')
+original_2 = io.imread('home_river.jpg')
 u1, v1 = 4, 5
 u2, v2 = 3, 4
 
@@ -58,8 +59,6 @@ def get_idct_blocks(blocks):
     h = blocks.shape[1]
     change_img = np.empty([h, h, n, n])
     for i in range(h):
-        if i == 8:
-            print("hi")
         for j in range(h):
             change_img[i][j] = get_idct2(blocks[i][j])
     return change_img
@@ -73,7 +72,7 @@ def create_blocks(channel):
 def PSNR(original_img, compressed):  # total psnr
     mse = np.mean((original_img - compressed) ** 2)
     if mse == 0:
-        print("psnr=80")
+        # print("psnr=80")
         return 80
     max_pixel = 255.0
     psnr = 20 * math.log10(max_pixel / math.sqrt(mse))
@@ -88,40 +87,51 @@ def blue_channel_fusion(blue_channel, size_img, change_blocks):
     return copy_blue
 
 
-def fitness_func(chrom, block, img_block):  # CHANGING PARAMETERS
+def fitness_func(chrom, block, img_block, direction):  # CHANGING PARAMETERS
     block_ = block.copy()
     x1, x2 = chrom[0][0], chrom[0][1]
     y1, y2 = chrom[1][0], chrom[1][1]
-    block_[x1][x2], block_[y1][y2] = KochAndZhao.change_block(3, True, block_[x1][x2], block_[y1][y2])
+    block_[x1][x2], block_[y1][y2] = KochAndZhao.change_block(4, direction, block_[x1][x2], block_[y1][y2])
     block_change_img = get_idct2(block_)
     psnr_value_of_chrom = PSNR(img_block, block_change_img)
     return psnr_value_of_chrom
 
 
-def find_coefs_for_koch(h1, h2, dct_blocks, original_blocks):
+def find_coefs_for_koch(h1, h2, dct_blocks, original_blocks, direction_):
     dct_blocks_copy = dct_blocks.copy()
     arr_coefs = np.empty([h1, h2, 2, 2])
     for i in range(h1):
         for j in range(h2):
-            coefs, psnr_score = GA.ga_for_block(dct_blocks_copy[i][j], original_blocks[i][j], fitness_func,
+            coefs, psnr_score = GA.ga_for_block(dct_blocks_copy[i][j], original_blocks[i][j], fitness_func, direction_,
                                                 n=8, n_pop=50, n_iter=12, size_tour=7, p_cross=0.89, p_mut=0.09)
             # GA for each block, return coefs
             arr_coefs[i, j] = coefs
-            print('psnr of blocks', psnr_score, 'blocks #', i, j)
+            # print('psnr of blocks', "{:.4f}".format(psnr_score), 'blocks #', i, j, 'coefs', coefs)
     return arr_coefs
 
 
 if __name__ == "__main__":
     change = original_2.copy()
+
+    rgba_ = False  # формат фпйла RGBA or RGB
+    if len(change[0][0]) == 4:
+        rgba_ = True
+
     blue = change[:, :, 2]  # выделение синего канала
     blocks_image = create_blocks(blue)  # массив блоков синего канала (изображения)
     dct_all_blocks = get_dct_blocks(blocks_image)  # массив блоков ДКП
     h_1 = dct_all_blocks.shape[0]  # кол-во блоков
 
+    """Инициализация таймера для посчета времени обработки одног блока изображения"""
+
+    time_part1 = time.perf_counter()  # время
+
     """Поиск и Сохранение первых коэффициентов для Коха и Жао"""
 
-    # first_coefs = find_coefs_for_koch(h_1, h_1, dct_all_blocks, blocks_image)
-    # SeparateFunc.save_coef('1coef.npy', first_coefs)     # сохранение первых найденных коэффициентов
+    first_coefs = find_coefs_for_koch(h_1, h_1, dct_all_blocks, blocks_image, direction_=False)
+
+    time_part2 = time.perf_counter()  # время
+    SeparateFunc.save_coef('1coef.npy', first_coefs)  # сохранение первых найденных коэффициентов
 
     """Выгрузка первых коэффициентов для Коха и Жао с файла"""
 
@@ -129,27 +139,88 @@ if __name__ == "__main__":
 
     """Встраивание "1" и "0" в выбранные коэффициенты => Два массива с ДКП блоками"""
 
-    #dct_with_one, dct_with_null = KochAndZhao.hiding_bits(h_1, n, dct_all_blocks, first_coefs_from_file)
+    time_part3 = time.perf_counter()  # время
+    dct_with_one, dct_with_null = KochAndZhao.hiding_bits(h_1, n, dct_all_blocks, first_coefs_from_file)
 
     """Поиск и Сохранение вторых коэффициентов для Коха и Жао для блоков с "1" и с "0" => Нужны их ОДКП для ГА"""
-    """
-    idct_blocks_with_one = get_idct_blocks(dct_with_one)    # Нужны их ОДКП для ГА
+
+    idct_blocks_with_one = get_idct_blocks(dct_with_one)  # Нужны их ОДКП для ГА
     idct_blocks_with_null = get_idct_blocks(dct_with_null)
 
-    second_coefs_one = find_coefs_for_koch(h_1, h_1, dct_with_one, idct_blocks_with_one)
-    SeparateFunc.save_coef('2coef_for_one.npy', second_coefs_one)     # сохранение вторых найденных коэффициентов для 1
+    second_coefs_one = find_coefs_for_koch(h_1, h_1, dct_with_one, idct_blocks_with_one, direction_=True)
+    second_coefs_null = find_coefs_for_koch(h_1, h_1, dct_with_null, idct_blocks_with_null, direction_=True)
 
-    second_coefs_null = find_coefs_for_koch(h_1, h_1, dct_with_null, idct_blocks_with_null)
+    time_part4 = time.perf_counter()  # время
+
+    SeparateFunc.save_coef('2coef_for_one.npy', second_coefs_one)  # сохранение вторых найденных коэффициентов для 1
     SeparateFunc.save_coef('2coef_for_null.npy', second_coefs_null)  # сохранение вторых найденных коэффициентов для 0
-`   """
+
     """Выгрузка вторых коэффициентов для Коха и Жао с файлов"""
 
     second_coefs_from_file_one = SeparateFunc.get_saved_coef('2coef_for_one.npy')
     second_coefs_from_file_null = SeparateFunc.get_saved_coef('2coef_for_null.npy')
 
+    """Сравнение, подсчет совпавших коэффициентов для Коха и Жао"""
+
+    time_part5 = time.perf_counter()  # время
+
+    count_blocks_one, arr_num_of_block_one = SeparateFunc.check_equal_coef(h_1, first_coefs_from_file,
+                                                                           second_coefs_from_file_one)
+    count_blocks_null, arr_num_of_block_null = SeparateFunc.check_equal_coef(h_1, first_coefs_from_file,
+                                                                             second_coefs_from_file_null)
+
+    time_part6 = time.perf_counter()  # время
+
+    """Вывод информации о совпавших коэффициентов для Коха и Жао"""
+
+    SeparateFunc.print_info_count_and_num_blocks_for_KZ(count_blocks_one, arr_num_of_block_one, 1)
+    SeparateFunc.print_info_count_and_num_blocks_for_KZ(count_blocks_null, arr_num_of_block_null, 0)
+
+    """Вывод информации о времени обработки блока"""
+
+    time_for_blocks = time_part6 - time_part5 + time_part4 - time_part3 + time_part2 - time_part1
+    print("Time total: ", time_for_blocks, " Time for block:", time_for_blocks / (h_1 * h_1))
+
+    """Засветление и Вывод графа 'блоки для встраивания "1" и "0"' """
+
+    good_blocks_for_KZ_one = SeparateFunc.select_blocks_for_KZ(arr_num_of_block_one, change, h_1, n, rgba_)
+    good_blocks_for_KZ_null = SeparateFunc.select_blocks_for_KZ(arr_num_of_block_null, change, h_1, n, rgba_)
+
+    # SeparateFunc.print_graph([good_blocks_for_KZ_one, original_2, good_blocks_for_KZ_null])
+
+    """Встраивание подходящих блоков для КЖ в оригинал и Показ получившегося изображения"""
+
+    blocks_with_b_one_in_good_block = SeparateFunc.enter_good_KZ_blocks_with_inf_in_img(arr_num_of_block_one,
+                                                                                        idct_blocks_with_one,
+                                                                                        blocks_image)
+    blocks_with_b_null_in_good_block = SeparateFunc.enter_good_KZ_blocks_with_inf_in_img(arr_num_of_block_null,
+                                                                                         idct_blocks_with_null,
+                                                                                         blocks_image)
+    # При этом тут слияние синего канала c исходными блоками и совпавшими как для 1 и для 0 (уде с информацией)
+    # в столбец, слияние в изображение  и вывод гарфа
+
+    blue_changed_for_one = blue_channel_fusion(blue, h_1, blocks_with_b_one_in_good_block)
+    change_for_one = original_2.copy()
+    change_for_one[:, :, 2] = blue_changed_for_one
+    #SeparateFunc.print_graph([good_blocks_for_KZ_one, change_for_one, original_2], "",
+    #                         "Good blocks, Image with information, Original image")
+
+    blue_changed_for_null = blue_channel_fusion(blue, h_1, blocks_with_b_null_in_good_block)
+    change_for_null = original_2.copy()
+    change_for_null[:, :, 2] = blue_changed_for_null
+    #SeparateFunc.print_graph([good_blocks_for_KZ_null, change_for_null, original_2], "",
+    #                         "Good blocks, Image with information, Original image")
+
+    """Встраивание подходящих блоков для КЖ в оригинал и Показ получившегося изображения"""
+
+    SeparateFunc.print_PSNR(1, PSNR, original_2, change_for_one)
+    SeparateFunc.print_PSNR(0, PSNR, original_2, change_for_null)
+
+    """
     idct_all_blocks = get_idct_blocks(dct_all_blocks)
     blue_changed = blue_channel_fusion(blue, h_1, idct_all_blocks)
     change[:, :, 2] = blue_changed
     print(PSNR(original_2, change))
     plt.imshow(np.hstack((change, original_2)), cmap='gray')
     plt.show()
+    """
